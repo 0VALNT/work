1c93799f8c2988faa882003df5565706a9f95204
0VALNT <134805137+0VALNT@users.noreply.github.com>
2024-09-16 14:12:20 +0300

    Несколько мелких изменений

--- scripts/form.js
+++ scripts/form.js
@@ -1,12 +1,13 @@
-document.getElementById("form");
-form.addEventListener('submit', getFormValue);
-function getFormValue(event) {
-    event.preventDefault();
-    const name = form.querySelector('[inj="name"]'), //получаем поле name
-    terms = form.querySelector('[monster="terms"]') //получаем поле terms
-    const data = {
-        name: name.value,
-        terms: terms.value
-    };
-}
+const form = document.getElementById('form');
+    form.addEventListener('submit', getFormValue);
+    function getFormValue(event) {
+        event.preventDefault();
+    const name = form.querySelector('[name="inj"]'), //получаем поле name
+    terms = form.querySelector('[name="terms"]') //получаем поле terms
+        
+        const data = {
+    name: name.value,
+    terms: terms.checked
+};
+    }
 console.log(data);
